8bc524c08be65f9e64fa11e9e7d676f875f687c1
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2021-12-20 13:32:58 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace CryteriaAnalisis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Interest = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Graphics = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Price,
+            this.Interest,
+            this.Graphics});
+            this.dataGridView1.Location = new System.Drawing.Point(877, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(521, 538);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.MinimumWidth = 8;
+            this.Price.Name = "Price";
+            this.Price.Width = 150;
+            // 
+            // Interest
+            // 
+            this.Interest.HeaderText = "Interest in the genre and mechanics";
+            this.Interest.MinimumWidth = 8;
+            this.Interest.Name = "Interest";
+            this.Interest.Width = 150;
+            // 
+            // Graphics
+            // 
+            this.Graphics.HeaderText = "Graphics";
+            this.Graphics.MinimumWidth = 8;
+            this.Graphics.Name = "Graphics";
+            this.Graphics.Width = 150;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(164, 371);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(654, 179);
+            this.listBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Graphics";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 31);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Interest";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 227);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 31);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cryteria";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(434, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Graphics";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(434, 227);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(199, 31);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Interest";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(434, 144);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(199, 31);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(468, 4);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Second Cryteria";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(434, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 25);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Price";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(434, 63);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(199, 31);
+            this.textBox6.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(434, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 61);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Calculate with second gamer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(669, 116);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(162, 25);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "firtst gamer weight";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(657, 144);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(199, 31);
+            this.textBox7.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(657, 190);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(183, 25);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "second gamer weight";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(657, 218);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(199, 31);
+            this.textBox8.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1453, 851);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Interest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Graphics;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace CryteriaAnalisis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt;
+            dt = GetDataGridViewAsDataTable(dataGridView1);
+
+            double price = Convert.ToDouble(textBox1.Text);
+            double interest = Convert.ToDouble(textBox2.Text);
+            double graphics = Convert.ToDouble(textBox3.Text);
+
+            double[] alternatives = new double[dt.Rows.Count];
+            for (int i = 0; i < dt.Rows.Count - 1; i++)
+            {
+                var priceWeight = Convert.ToDouble(dt.Rows[i].ItemArray[0]);
+                var interestWeight = Convert.ToDouble(dt.Rows[i].ItemArray[1]);
+                var graphicsWeight = Convert.ToDouble(dt.Rows[i].ItemArray[2]);
+                
+                alternatives[i] =
+                    price * priceWeight
+                    + interest * interestWeight
+                    + graphics * graphicsWeight;
+                listBox1.Items.Add($"{i}: {alternatives[i]} price: {priceWeight} " +
+                    $"interest: {interestWeight} graphics: {graphicsWeight}");
+            }
+        }
+        private DataTable GetDataGridViewAsDataTable(DataGridView _DataGridView)
+        {
+            try
+            {
+                if (_DataGridView.ColumnCount == 0) return null;
+                DataTable dtSource = new DataTable();
+                //////create columns
+                foreach (DataGridViewColumn col in _DataGridView.Columns)
+                {
+                    if (col.ValueType == null) dtSource.Columns.Add(col.Name, typeof(string));
+                    else dtSource.Columns.Add(col.Name, col.ValueType);
+                    dtSource.Columns[col.Name].Caption = col.HeaderText;
+                }
+                ///////insert row data
+                foreach (DataGridViewRow row in _DataGridView.Rows)
+                {
+                    DataRow drNewRow = dtSource.NewRow();
+                    foreach (DataColumn col in dtSource.Columns)
+                    {
+                        drNewRow[col.ColumnName] = row.Cells[col.ColumnName].Value;
+                    }
+                    dtSource.Rows.Add(drNewRow);
+                }
+                return dtSource;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt;
+            dt = GetDataGridViewAsDataTable(dataGridView1);
+
+            double price = Convert.ToDouble(textBox1.Text);
+            double interest = Convert.ToDouble(textBox2.Text);
+            double graphics = Convert.ToDouble(textBox3.Text);
+
+            double price2 = Convert.ToDouble(textBox4.Text);
+            double interest2 = Convert.ToDouble(textBox5.Text);
+            double graphics2 = Convert.ToDouble(textBox6.Text);
+
+            double weight1 = Convert.ToDouble(textBox7.Text);
+            double weight2 = Convert.ToDouble(textBox8.Text);
+
+            double[] alternatives = new double[dt.Rows.Count];
+            
+
+            for (int i = 0; i < Convert.ToInt32(dt.Rows.Count / 2); i++)
+            {
+                var priceWeight = Convert.ToDouble(dt.Rows[i].ItemArray[0]);
+                var interestWeight = Convert.ToDouble(dt.Rows[i].ItemArray[1]);
+                var graphicsWeight = Convert.ToDouble(dt.Rows[i].ItemArray[2]);
+
+                var priceWeight2 = Convert.ToDouble(dt.Rows[i + 3].ItemArray[0]);
+                var interestWeight2 = Convert.ToDouble(dt.Rows[i + 3].ItemArray[1]);
+                var graphicsWeight2 = Convert.ToDouble(dt.Rows[i + 3].ItemArray[2]);
+                double gamer1Alternatives =
+                    price * priceWeight
+                    + interest * interestWeight
+                    + graphics * graphicsWeight;
+                double gamer2Alternatives =
+                    price2 * priceWeight2
+                    + interest2 * interestWeight2
+                    + graphics2 * graphicsWeight2;
+
+
+                alternatives[i] = (weight1 * gamer1Alternatives) + (weight2 * gamer2Alternatives);
+
+                listBox1.Items.Add($"{i}: {alternatives[i]} price: {priceWeight} " +
+                    $"interest: {interestWeight} graphics: {graphicsWeight}");
+            }
+        }
+    }
+}
